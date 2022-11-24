a4d4977693e0c26ea2fc4fc2de7e7c02c31aa60a
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-23 19:25:30 -0500

    added background image

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -13,7 +13,7 @@ return (
         I am Alexander Lin, a Junior Fullstack Developer Student and a United States Marine. In my lesiure, I enjoy listening to music, play call of duty, and workout. I am also a current student at Kennesaw State University majoring in Computer Science. From here, I wish to seek for full time opportunities in Fullstack Development while obtaining a bachelor's in computer science! 
       </p>
     </div>
-  );
+);
 }
 
 export default About;
