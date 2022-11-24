e3b6508e262ad14d5858e4376b1518275e4bc5b5
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-23 19:07:09 -0500

    updated app.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,14 +1,15 @@
 import React from "react";
+import selfie from '../IMG/selfie.png';
 
 function About() {
-  return (
-    <div>
-      <p className="content is-medium"> Junior Fullstack Developer Student </p>
-      <hr />
-      <img className="pic" src={process.env.PUBLIC_URL + './public/IMG_0553.jpg'} alt="selfie"/>
+return (
+  <div>
+    <p className="content is-medium"> Junior Fullstack Developer Student </p>
+    <hr />
+    <img className="pic" src={selfie} alt="selfie" />
+     
 
-      
-      <p className="About-Me">
+    <p className="About-Me">
         I am Alexander Lin, a Junior Fullstack Developer Student and a United States Marine. In my lesiure, I enjoy listening to music, play call of duty, and workout. I am also a current student at Kennesaw State University majoring in Computer Science. From here, I wish to seek for full time opportunities in Fullstack Development while obtaining a bachelor's in computer science! 
       </p>
     </div>
